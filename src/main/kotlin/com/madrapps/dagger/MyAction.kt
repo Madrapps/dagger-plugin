875d4dc704febfcbd115b69ab66f72dd65297613
thsaravana <th.saravana@gmail.com>
2020-03-31 22:20:21 +0530

    Passing the BindingGraphPlugin along with the Processor

--- src/main/kotlin/com/madrapps/dagger/MyAction.kt
+++ src/main/kotlin/com/madrapps/dagger/MyAction.kt
@@ -4,71 +4,89 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.roots.OrderEnumerator
 import com.intellij.util.PathUtil
-import com.sun.tools.javac.api.JavacTool
-import com.sun.tools.javac.resources.compiler
 import dagger.internal.DaggerCollections
 import dagger.internal.codegen.ComponentProcessor
 import org.jetbrains.kotlin.idea.util.projectStructure.allModules
 import org.jetbrains.kotlin.idea.util.sourceRoots
 import java.io.File
-import java.util.*
 import javax.tools.*
 import javax.tools.JavaCompiler.CompilationTask
+import kotlin.system.measureTimeMillis
 
 
 class MyAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        println("TTRRTT, ActionPerformed")
+        val lo = measureTimeMillis {
+            println("TTRRTT, ActionPerformed")
 
-        val project = e.project ?: throw IllegalArgumentException()
-        val sourceFolder = project.allModules().flatMap { it.sourceRoots.toList() }
-            .filter {
-                it.canonicalPath?.endsWith("src/main/java") == true
-                        // || it.canonicalPath?.endsWith("src/main/kotlin") == true
+            val project = e.project ?: throw IllegalArgumentException()
+            val sourceFolder = project.allModules().flatMap { it.sourceRoots.toList() }
+                .filter {
+                    it.canonicalPath?.endsWith("src/main/java") == true
+                    // || it.canonicalPath?.endsWith("src/main/kotlin") == true
+                }
+            println(sourceFolder)
+
+
+            val compiler = ToolProvider.getSystemJavaCompiler()
+            val diagnostics = DiagnosticCollector<JavaFileObject>()
+            val fileManager: StandardJavaFileManager = compiler.getStandardFileManager(diagnostics, null, null)
+            val outputDirectory = File(project.basePath, "build/dagger-plugin")
+            outputDirectory.mkdirs()
+
+            fileManager.setLocation(StandardLocation.CLASS_OUTPUT, listOf(outputDirectory))
+            println("--- | " + fileManager.getLocation(StandardLocation.CLASS_OUTPUT).toList())
+
+            val mutableListOf = mutableListOf<JavaFileObject>()
+            val javaObjects =
+                File(sourceFolder.first().canonicalPath).walkTopDown().filter { it.isFile && it.extension == "java" }
+                    .forEach {
+                        mutableListOf += fileManager.getJavaFileObjects(it)
+                    }
+
+            val modules = project.allModules().filter { it.sourceRoots.isNotEmpty() }
+
+            val module = project.allModules().get(1)
+            val files: MutableList<File> = OrderEnumerator.orderEntries(module).recursively().pathsList.virtualFiles
+                .filterNot { it.path.contains("com.google.dagger") }
+                .map { File(it.path) }.toMutableList()
+
+            val jarPathForClass = PathUtil.getJarPathForClass(DaggerCollections::class.java)
+
+            files.add(File(jarPathForClass))
+            fileManager.setLocation(StandardLocation.CLASS_PATH, files)
+            val classRoots = OrderEnumerator.orderEntries(module).classesRoots.filter { it.path.endsWith("main") }
+                .map { File(it.path) }
+            val classes = mutableListOf<String>()
+            classRoots.forEach {
+                it.walkTopDown().forEach {
+                    classes += it.path
+                }
             }
-        println(sourceFolder)
 
+            val map = mutableListOf.map {
+                it.name
+            }
 
-
-        val compiler = ToolProvider.getSystemJavaCompiler()
-        val diagnostics = DiagnosticCollector<JavaFileObject>()
-        val fileManager: StandardJavaFileManager = compiler.getStandardFileManager(diagnostics, null, null)
-        fileManager.setLocation(StandardLocation.CLASS_OUTPUT, listOf(File(project.basePath, "build/dagger-plugin")))
-        println("--- | " + fileManager.getLocation(StandardLocation.CLASS_OUTPUT).toList())
-
-        val mutableListOf = mutableListOf<JavaFileObject>()
-        val javaObjects = File(sourceFolder.first().canonicalPath).walkTopDown().filter { it.isFile && it.extension =="java" }.forEach {
-            mutableListOf += fileManager.getJavaFileObjects(it)
-        }
-        val module = project.allModules().get(1)
-        val files: MutableList<File> = OrderEnumerator.orderEntries(module).recursively().pathsList.virtualFiles
-            .filterNot { it.path.contains("com.google.dagger") }
-            .map { File(it.path) }.toMutableList()
-
-        val jarPathForClass = PathUtil.getJarPathForClass(DaggerCollections::class.java)
-        files.add(File(jarPathForClass))
-        fileManager.setLocation(StandardLocation.CLASS_PATH, files)
-        val classRoots = OrderEnumerator.orderEntries(module).classesRoots.filter { it.path.endsWith("main") }.map { File(it.path) }
-        val classes = mutableListOf<String>()
-        classRoots.forEach {
-            it.walkTopDown().forEach {
-               classes += it.path
+            val task: CompilationTask =
+                compiler.getTask(
+                    null,
+                    fileManager,
+                    diagnostics,
+                    null,
+                    null,
+                    mutableListOf
+                )
+            val specPlugin = SpewPlugin()
+            val forTesting = ComponentProcessor.forTesting(specPlugin)
+            task.setProcessors(
+                listOf(forTesting)
+            )
+            val success = task.call()
+            for (diagnostic in diagnostics.diagnostics) {
+                // System.err.println(diagnostic)
             }
         }
-
-        val map = mutableListOf.map {
-            it.name
-        }
-
-        val task: CompilationTask =
-            compiler.getTask(null, fileManager, diagnostics, null, listOf("com.madrapps.dagger.component.EmptyComponent"), null)
-        val componentProcessor = ComponentProcessor()
-        task.setProcessors(
-            listOf(componentProcessor)
-        )
-        val success = task.call()
-        for (diagnostic in diagnostics.diagnostics) {
-            System.err.println(diagnostic)
-        }
+        println(" LO $lo")
     }
 }
---
+++ src/main/kotlin/com/madrapps/dagger/SpewPlugin.kt
@@ -0,0 +1,32 @@
+package com.madrapps.dagger
+
+import com.squareup.javapoet.ClassName
+import dagger.model.BindingGraph
+import dagger.spi.BindingGraphPlugin
+import dagger.spi.DiagnosticReporter
+import java.util.stream.Collectors
+import javax.annotation.processing.Filer
+import javax.lang.model.util.Elements
+import javax.lang.model.util.Types
+
+class SpewPlugin : BindingGraphPlugin {
+
+    override fun visitGraph(
+        bindingGraph: BindingGraph,
+        diagnosticReporter: DiagnosticReporter
+    ) {
+        val rootComponentNode = bindingGraph.rootComponentNode()
+        println("RootComponent - $rootComponentNode")
+        val componentElement =
+            rootComponentNode.componentPath().currentComponent()
+        val componentName = ClassName.get(componentElement)
+        val dumb: String? = null
+        //        dumb.equals(dumb);
+        //println(" - |-| - " + componentName.simpleName())
+        val map = bindingGraph.bindings().map { it.key().type() }
+        println("Keys = $map")
+        for (node in bindingGraph.bindings().stream().distinct().collect(Collectors.toList())) {
+            //println(" --- " + node.javaClass.simpleName)
+        }
+    }
+}
