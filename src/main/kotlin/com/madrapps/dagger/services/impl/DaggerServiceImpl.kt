cc6bf133d3271797340559364b5a5f194ea471dd
thsaravana <th.saravana@gmail.com>
2020-04-06 19:54:20 +0530

    Adding IntoMap StringKey's along with the Type

--- src/main/kotlin/com/madrapps/dagger/services/impl/DaggerServiceImpl.kt
+++ src/main/kotlin/com/madrapps/dagger/services/impl/DaggerServiceImpl.kt
@@ -7,9 +7,12 @@ import com.madrapps.dagger.*
 import com.madrapps.dagger.services.DaggerService
 import com.madrapps.dagger.services.service
 import com.madrapps.dagger.toolwindow.DaggerNode
+import com.sun.tools.javac.code.Attribute
 import com.sun.tools.javac.code.Symbol
 import com.sun.tools.javac.code.Type
+import dagger.model.Binding
 import dagger.model.BindingGraph
+import dagger.model.BindingKind
 import dagger.model.DependencyRequest
 import javax.lang.model.element.Element
 import javax.swing.tree.DefaultMutableTreeNode
@@ -34,7 +37,7 @@ class DaggerServiceImpl(private val project: Project) : DaggerService {
                 DaggerNode(project, rootComponentNode.name, rootComponentNode.toPsiClass(project)!!, null, "entry")
 
             rootComponentNode.entryPoints().forEach {
-                addNodes(it, bindingGraph, componentNode, true)
+                addNodes(it, bindingGraph, componentNode, null, true)
             }
 
             var root = project.service.treeModel.root
@@ -51,24 +54,40 @@ class DaggerServiceImpl(private val project: Project) : DaggerService {
         dr: DependencyRequest,
         bindingGraph: BindingGraph,
         parentNode: DaggerNode,
+        parentBinding: Binding?,
         isEntryPoint: Boolean
     ) {
         var currentNode = parentNode
         val key = dr.key()
         val binding = bindingGraph.bindings(key).first()
-        binding.bindingElement().ifPresent { element ->
-            val daggerNode = dr.createNode(element, isEntryPoint, "${dr.kind()} / ${binding.kind()}")
-            if (daggerNode != null) {
-                currentNode = daggerNode
+        if (binding.kind() == BindingKind.MULTIBOUND_MAP) {
+            val classType = dr.key().type() as? Type.ClassType
+            if (classType != null) {
+                val name = classType.presentableName()
+                val psiElement = (dr.requestElement().orNull() as Symbol.VarSymbol).toPsiParameter(project)!!
+                currentNode = DaggerNode(project, name, psiElement, null, "${dr.kind()} / ${binding.kind()}")
                 parentNode.add(currentNode)
+            }
+        } else {
+            binding.bindingElement().ifPresent { element ->
+                val daggerNode = dr.createNode(element, isEntryPoint, parentBinding, "${dr.kind()} / ${binding.kind()}")
+                if (daggerNode != null) {
+                    currentNode = daggerNode
+                    parentNode.add(currentNode)
+                }
             }
         }
         binding.dependencies().forEach {
-            addNodes(it, bindingGraph, currentNode, false)
+            addNodes(it, bindingGraph, currentNode, binding, false)
         }
     }
 
-    private fun DependencyRequest.createNode(element: Element, isEntryPoint: Boolean, type: String): DaggerNode? {
+    private fun DependencyRequest.createNode(
+        element: Element,
+        isEntryPoint: Boolean,
+        parentBinding: Binding?,
+        type: String
+    ): DaggerNode? {
         val name: String
         val psiElement: PsiElement
         when (element) {
@@ -78,11 +97,17 @@ class DaggerServiceImpl(private val project: Project) : DaggerService {
             }
             is Symbol.MethodSymbol -> {
                 psiElement = element.toPsiMethod(project)!!
-                name = requestElement().orNull()?.name() ?: if (psiElement.isConstructor) {
+                var temp = requestElement().orNull()?.name() ?: if (psiElement.isConstructor) {
                     psiElement.name
                 } else {
                     psiElement.returnType?.presentableText ?: "NULL"
                 }
+                if (parentBinding?.kind() == BindingKind.MULTIBOUND_MAP) {
+                    temp += " [${(element.annotationMirrors.find {
+                        (it.annotationType as Type.ClassType).tsym.qualifiedName.toString() == "dagger.multibindings.StringKey"
+                    } as Attribute.Compound).values.first().snd}]"
+                }
+                name = temp
             }
             is Symbol.VarSymbol -> {
                 psiElement = element.toPsiParameter(project)!!
@@ -113,15 +138,16 @@ class DaggerServiceImpl(private val project: Project) : DaggerService {
         return if (this is Symbol.MethodSymbol) {
             returnType.tsym.simpleName.toString()
         } else if (this is Symbol.VarSymbol) {
-            val classType = type as? Type.ClassType
-            if (classType != null) {
-                var name = classType.tsym.simpleName.toString()
-                val params = classType.typarams_field
-                if (params.isNotEmpty()) {
-                    name += "<${params.joinToString(",") { it.tsym.simpleName }}>"
-                }
-                return name
-            } else null
+            (type as? Type.ClassType)?.presentableName()
         } else null
+    }
+
+    private fun Type.ClassType.presentableName(): String {
+        var name = tsym.simpleName.toString()
+        val params = typarams_field
+        if (params.isNotEmpty()) {
+            name += "<${params.joinToString(",") { it.tsym.simpleName }}>"
+        }
+        return name
     }
 }
