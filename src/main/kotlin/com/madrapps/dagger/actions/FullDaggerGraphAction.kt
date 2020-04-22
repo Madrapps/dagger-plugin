6f5b53d0cc409a40d682e08c9ba3bd4e02376124
thsaravana <th.saravana@gmail.com>
2020-04-22 17:14:12 +0530

    Implementing full dagger view

--- src/main/kotlin/com/madrapps/dagger/actions/FullDaggerGraphAction.kt
+++ src/main/kotlin/com/madrapps/dagger/actions/FullDaggerGraphAction.kt
@@ -1,8 +1,10 @@
 package com.madrapps.dagger.actions
 
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.madrapps.dagger.core.createChildTree
 import com.madrapps.dagger.services.log
 import com.madrapps.dagger.services.service
+import javax.swing.tree.DefaultMutableTreeNode
 
 class FullDaggerGraphAction : AbstractViewAction() {
 
@@ -15,8 +17,18 @@ class FullDaggerGraphAction : AbstractViewAction() {
     override fun setSelected(e: AnActionEvent, state: Boolean) {
         val project = e.project
         if (project != null) {
-            project.log("View Full Dagger graph")
-            project.service.viewToggler().select(ID)
+            if (!isSelected(e)) {
+                project.log("View Full Dagger graph")
+                project.service.viewToggler().select(ID)
+
+                val treeModel = project.service.treeModel
+                val root = DefaultMutableTreeNode("")
+                treeModel.setRoot(root)
+                project.service.nodes.filter { it.parents.isEmpty() }.forEach {
+                    root.add(it.createChildTree(project, ""))
+                    treeModel.reload()
+                }
+            }
         }
     }
 }
